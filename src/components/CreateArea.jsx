9bdc7149d2a7bc62bf27ba12ac7665a4aeb8e6e1
sreerajpr06 <sreerajpr06@gmail.com>
2021-02-01 23:25:00 +0530

    added animation for creating new note

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
+import Zoom from "@material-ui/core/Zoom";
 
 function CreateArea(props){
+    let [isExpanded, setIsExpanded] = useState(false);
+
     let [newItem, setNewItem] = useState({
         title: "",
         content: ""
@@ -38,14 +41,29 @@ function CreateArea(props){
         <div className="add-content-container">
             <form>
                 <div className="add-content">
-                    <input onChange={handleInputChange} name="title" placeholder="Title" value={newItem.title} />
-                    <textarea onChange={handleInputChange} name="content" placeholder="Content" rows="3" value={newItem.content} />
-                    <button onClick={ (event) => {
-                        props.onAdd(newItem);
-                        newItem.title = "";
-                        newItem.content = "";
-                        event.preventDefault();
-                    }}>+</button>
+                    {isExpanded && <input 
+                        onChange={handleInputChange} 
+                        name="title" 
+                        placeholder="Title" 
+                        value={newItem.title} 
+                    />}
+                    <textarea 
+                        onClick={ () => setIsExpanded(true)} 
+                        onChange={handleInputChange} 
+                        name="content" 
+                        placeholder="Content" 
+                        rows={isExpanded ? 3 : 1} 
+                        value={newItem.content} 
+                    />
+                    
+                    <Zoom in={isExpanded}>
+                        <button onClick={ (event) => {
+                            props.onAdd(newItem);
+                            newItem.title = "";
+                            newItem.content = "";
+                            event.preventDefault();
+                        }}>+</button>
+                    </Zoom>
                 </div>    
                 
             </form>
